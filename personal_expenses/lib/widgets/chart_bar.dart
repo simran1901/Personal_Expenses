adfcfd1848e364bd95fc9796d5556e52b3a1c9c2
Simran Makhijani <64222207+simran1901@users.noreply.github.com>
2020-10-31 02:30:05 +0530

    Update chart_bar.dart

--- personal_expenses/lib/widgets/chart_bar.dart
+++ personal_expenses/lib/widgets/chart_bar.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      children: <Widget>[
+        
+      ],
     );
   }
 }
