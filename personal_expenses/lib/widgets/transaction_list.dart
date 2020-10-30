d701eca60390bacaab64ac53402272553ecbc144
simran1901 <simranmakhijani55@gmail.com>
2020-10-31 02:08:41 +0530

    added application

---
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget { 
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: transactions.isEmpty ? 
+          Column( 
+            children: <Widget>[
+              Text(
+                'No transaction added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/image/waiting.png', 
+                  fit: BoxFit.cover,
+                  )
+              ),
+            ],
+          ): 
+          ListView(
+                children: transactions.map((tx) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                              ),
+                            ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Rs. ${tx.amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            Text(
+                              DateFormat.yMMMd().format(tx.date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+    );
+  }
+}
