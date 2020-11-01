aa7acda57076218392e77c1bba21cc9fc60b80e5
simran1901 <simranmakhijani55@gmail.com>
2020-11-01 14:35:46 +0530

    updating chart_bar.dart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,9 +9,29 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Column(
       children: <Widget>[
-        
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(height: 4,),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+               ),
+            ],
+          ),
+        ),
+        SizedBox(height:4,),
       ],
     );
   }
